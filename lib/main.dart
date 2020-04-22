e6b7c740f5e735b4dc57fd9064448cda390460ee
Ehmad <justEhmadSaeed@gmail.com>
2020-04-23 00:47:33 +0500

    🐛 FIX: Remove redundant libraries

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'story_brain.dart';
-import 'story_brain.dart';
-import 'story_brain.dart';
-//TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
 
 void main() => runApp(Destini());
 
